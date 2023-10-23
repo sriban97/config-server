45e78b14542aefde38d85d9145388efc6ec58841
Sriban <sriban.mariyappan@gmail.com>
2023-10-23 21:29:56 +0530

    init commit

---
+++ src/test/java/com/green/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
